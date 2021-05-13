9a03e98e314f0a7f2780f8551ddf56726ddb8885
Michael Ebenal <willtreatyaraluen@gmail.com>
2021-05-13 03:05:11 -0600

    Structure for HiLo

---
+++ hilo/__main__.py
@@ -0,0 +1,77 @@
+# TODO: Add entry point code here
+
+class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to keep track of the score and control the 
+    sequence of play.
+    
+    Attributes:
+        keep_playing (boolean): Whether or not the player wants to keep playing.
+        score (number): The total number of points earned.
+        thrower (Thrower): An instance of the class of objects known as Thrower.
+    """
+
+    def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self.keep_playing = True
+        self.score = 0
+        self.dealer = Dealer()
+
+    def start_game(self):
+        """Starts the game loop to control the sequence of play.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.keep_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+        """Gets the inputs at the beginning of each round of play. In this case,
+        that means throwing the dice.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        
+        
+    def do_updates(self):
+        """Updates the important game information for each round of play. In 
+        this case, that means updating the score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        
+    def do_outputs(self):
+        """Outputs the important game information for each round of play. In 
+        this case, that means the dice that were rolled and the score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if True:
+            self.keep_playing = True
+        else:
+            self.keep_playing = False
+
+class Dealer:
+  
+  cards = []
+  curr_card
+  prev_card
+
+  def __init__():
+    pass
+
+  def get_points(self, guess):
+    pass
+
+  def get_card(self):
+    pass
