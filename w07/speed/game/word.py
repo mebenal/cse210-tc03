ca662528605ca72f3ce35fbd387d32da7eb6f329
laxmeyers <mey18003@byui.edu>
2021-06-07 10:53:11 -0600

    made word

--- w07/speed/game/word.py
+++ w07/speed/game/word.py
@@ -6,12 +6,20 @@ from game.point import Point
 class Word(Actor):
 
     def __init__(self):
-        select = []
-        with open("words.txt") as words:
-            for i in range(len(words)):
-                select.append(words[i])
-        num = random.randint(0, len(words))
-        self.word = select[num]
+        super().__init__()
+        y = random.randint(2, constants.MAX_Y - 2)
+        self.set_position(Point(0, y))
+        self.set_velocity(Point(1,0))
+        num = random.randint(0, len(constants.LIBRARY))
+        self._word = constants.LIBRARY[num]
+
 
     def reset_word(self):
-        None
+        num = random.randint(0, len(constants.LIBRARY))
+        self._word = constants.LIBRARY[num]
+
+    def get_word(self):
+        return self._word
+    
+    def set_word(self, word):
+        self._word = word
