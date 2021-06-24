905495b792fd83ffb0930eb27df347075da132b1
JARROD WISE <wis20003@byui.edu>
2021-06-24 16:47:08 -0600

    interactionmoveenemy

--- finalGame/game/enemy.py
+++ finalGame/game/enemy.py
@@ -7,6 +7,9 @@ class Enemy(arcade.Sprite):
     super().__init__()
     self.__dict__ = sprite.__dict__.copy()
     self._set_scale(constants.CHARACTER_SCALING)
+    self._move_behavior = 0
 
-  def fight_or_flight(self, player, enemy):
-    arcade.Sprite.get_distance_between_sprites(player, enemy)
+  def set_move_behavior(self, new_behavior):
+    self._move_behavior = new_behavior
+
+  
